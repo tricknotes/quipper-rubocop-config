947783b3cf10aae473e84bf5939cb02ddb367cac
Drew Terry <drewpterry@yahoo.com>
2018-07-04 17:37:06 +0900

    install config file with client

--- lib/quipper/rubocop/config.rb
+++ lib/quipper/rubocop/config.rb
@@ -1,9 +1,19 @@
-require "quipper/rubocop/config/version"
+require 'thor'
 
 module Quipper
   module Rubocop
     module Config
-      # Your code goes here...
+      class CLI < Thor
+
+        CONFIG_FILE_NAME = ".rubocop.yml"
+        desc "install", "Install rubocop config"
+        def install
+          raise "usage: onkcop init" unless args.empty?
+          template_path = File.expand_path("../../../templates", __dir__)
+          puts "#{File.exist?(CONFIG_FILE_NAME) ? "overwrite" : "create"} #{CONFIG_FILE_NAME}"
+          FileUtils.copy_file(File.join(template_path, CONFIG_FILE_NAME), CONFIG_FILE_NAME)
+        end
+      end
     end
   end
 end
--- quipper-rubocop-config.gemspec
+++ quipper-rubocop-config.gemspec
@@ -29,8 +29,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "pronto", "~> 0.9.5"
+  spec.add_dependency "pronto", "0.9.5"
   spec.add_dependency "pronto-rubocop", "~> 0.9.0"
+  spec.add_dependency "rubocop", "~> 0.52.1"
+  spec.add_dependency "thor", "~> 0.19"
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
 end
