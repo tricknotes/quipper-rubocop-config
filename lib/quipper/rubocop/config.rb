933f63184b054ba115522b27c68668c2b187c4b5
Drew Terry <drewpterry@yahoo.com>
2018-07-04 18:44:43 +0900

    run rubocop with todo list

--- lib/quipper/rubocop/config.rb
+++ lib/quipper/rubocop/config.rb
@@ -6,12 +6,22 @@ module Quipper
       class CLI < Thor
 
         CONFIG_FILE_NAME = ".rubocop.yml"
+        GITHOOK_FILE_PATH = ".githook/prepush"
         desc "install", "Install rubocop config"
+        option :todo
         def install
-          raise "usage: onkcop init" unless args.empty?
           template_path = File.expand_path("../../../templates", __dir__)
           puts "#{File.exist?(CONFIG_FILE_NAME) ? "overwrite" : "create"} #{CONFIG_FILE_NAME}"
           FileUtils.copy_file(File.join(template_path, CONFIG_FILE_NAME), CONFIG_FILE_NAME)
+
+          puts "creating rubocop todo file"
+
+          system "rubocop --auto-gen-config" if options[:todo]
+
+          puts "adding git hook"
+          #puts "#{File.exist?(GITHOOK_FILE_PATH) ? "overwrite" : "create"} #{GITHOOK_FILE_PATH}"
+          #FileUtils.copy_file(File.join(template_path, GITHOOK_FILE_PATH), GITHOOK_FILE_PATH)
+
         end
       end
     end
